466c41f86d2d29ea53a57d662778445bf6524582
Aliaksandr Krukouski <AliaksandrKrukouski@gmail.com>
2017-07-03 22:08:33 +0300

    Sort class

---
+++ Task2/src/by/krukouski/train/action/Sort.java
@@ -0,0 +1,26 @@
+package by.krukouski.train.action;
+
+import java.util.Comparator;
+import by.krukouski.train.wagon.Wagon;
+
+public class Sort {
+	static final Comparator<Wagon> LENGTH_ASC = new Comparator<Wagon>() {
+		public int compare(Wagon wagonOne, Wagon wagonTwo) {
+			return wagonOne.getLength() - wagonTwo.getLength();
+		}
+	};
+	
+	static final Comparator<Wagon> WIDTH_ASC = new Comparator<Wagon>() {
+		public int compare(Wagon wagonOne, Wagon wagonTwo) {
+			return wagonOne.getWidth() - wagonTwo.getWidth();
+		}
+	};
+	
+	static final Comparator<Wagon> WEIGHT_ASC = new Comparator<Wagon>() {
+		public int compare(Wagon wagonOne, Wagon wagonTwo) {
+			return wagonOne.getWeight() - wagonTwo.getWeight();
+		}
+	};
+	
+	
+}
