8640ab271ae6f1ced223950116d015f870d49f76
Aliaksandr Krukouski <AliaksandrKrukouski@gmail.com>
2017-07-05 00:05:53 +0300

    JUnit for CountTrainCapacityTest

---
+++ Task2/src/by/krukouski/train/test/CountTrainSeatNumberTest.java
@@ -0,0 +1,31 @@
+package by.krukouski.train.test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import by.krukouski.train.action.TrainCount;
+import by.krukouski.train.exception.FileNotExistsException;
+import by.krukouski.train.exception.InvalidWagonTypeException;
+import by.krukouski.train.fileoperation.StringReader;
+import by.krukouski.train.train.Train;
+import by.krukouski.train.train.TrainBuilder;
+
+public class CountTrainSeatNumberTest {
+
+	@Test
+	public void test() throws IOException, FileNotExistsException, InvalidWagonTypeException {
+		String testDataFilePath = "resource/CountTrainSeatNumberTest.txt";
+		String testResultFilePath = "resource/CountTrainSeatNumberTestResult.txt";
+		
+		ArrayList<String> wagonListString = StringReader.readFileIntoList(testDataFilePath);
+		Train train = TrainBuilder.BuildTrainFromStringList(wagonListString);
+		
+		double expected = Double.parseDouble(StringReader.readFileIntoList(testResultFilePath).get(0));
+		double actual = TrainCount.countTrainSeatNumber(train);
+		
+		Assert.assertEquals(expected, actual, 0.01);
+	}
+}
