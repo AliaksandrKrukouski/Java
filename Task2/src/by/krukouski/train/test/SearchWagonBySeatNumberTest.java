5d1233b11398a5c44494f1269ac7b68c239d56fb
Aliaksandr Krukouski <AliaksandrKrukouski@gmail.com>
2017-07-05 01:10:23 +0300

    JUnit for SearchWagonBySeatNumberTest

---
+++ Task2/src/by/krukouski/train/test/SearchWagonBySeatNumberTest.java
@@ -0,0 +1,37 @@
+package by.krukouski.train.test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import by.krukouski.train.action.TrainSearch;
+import by.krukouski.train.exception.FileNotExistsException;
+import by.krukouski.train.exception.InvalidWagonTypeException;
+import by.krukouski.train.fileoperation.StringReader;
+import by.krukouski.train.train.Train;
+import by.krukouski.train.train.TrainBuilder;
+import by.krukouski.train.wagon.Wagon;
+
+public class SearchWagonBySeatNumberTest {
+
+	@Test
+	public void test() throws IOException, FileNotExistsException, InvalidWagonTypeException {
+		String testDataFilePath = "resource/SearchWagonBySeatNumberTest.txt";
+		String testResultFilePath = "resource/SearchWagonBySeatNumberTestResult.txt";
+		
+		ArrayList<String> expectedWagonListString = StringReader.readFileIntoList(testResultFilePath);
+		Train expectedTrain = TrainBuilder.BuildTrainFromStringList(expectedWagonListString);
+		ArrayList<Wagon> expectedWagons = expectedTrain.getTrain();  
+		
+		ArrayList<String> actualWagonListString = StringReader.readFileIntoList(testDataFilePath);
+		Train actualTrain = TrainBuilder.BuildTrainFromStringList(actualWagonListString);
+		ArrayList<Wagon> actualWagons = TrainSearch.searchWagonBySeatNumber(actualTrain, 10, 25);
+		
+		Object[] expectedArray = expectedWagons.toArray();
+		Object[] actualArray = actualWagons.toArray();
+		
+		Assert.assertArrayEquals(expectedArray, actualArray);
+	}
+}
