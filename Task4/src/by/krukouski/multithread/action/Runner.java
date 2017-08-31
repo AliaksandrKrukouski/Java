ad99a0c806315a40d90c87c3fe58c60978ca276e
Aliaksandr Krukouski <AliaksandrKrukouski@gmail.com>
2017-09-01 00:42:16 +0300

    Add SHIP_NUMBER constant

--- Task4/src/by/krukouski/multithread/action/Runner.java
+++ Task4/src/by/krukouski/multithread/action/Runner.java
@@ -7,9 +7,11 @@ public class Runner {
 
 	public static void main(String[] args) {
 		final int PORT_CAPACITY = 3; 
+		final int SHIP_NUMBER = 10; 
+		
 		LoadingPort port = LoadingPort.getPort(PORT_CAPACITY);
 		
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i <= SHIP_NUMBER; i++) {
 			new CargoShip(i, port).start();
 		}
 	}
